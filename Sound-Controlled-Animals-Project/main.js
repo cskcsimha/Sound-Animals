b24467269c36ffec198155f19b5b581dae53006d
cskcsimha <92619094+cskcsimha@users.noreply.github.com>
2022-01-23 12:58:10 +0530

    Add files via upload

---
+++ Sound-Controlled-Animals-Project/main.js
@@ -0,0 +1,12 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/LYa9kXK07/model.json', modelReady);
+ }
+ 
+ function modelReady(){
+     classifier.classify(gotResults);
+ }
+ 
+ function gotResults(error, results){
+     console.log('got results')
+ }
