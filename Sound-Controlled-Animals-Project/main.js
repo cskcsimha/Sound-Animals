5af1f8348d41e08687819e934786964df2b9a51a
cskcsimha <92619094+cskcsimha@users.noreply.github.com>
2022-01-29 11:01:33 +0530

    Add files via upload

--- Sound-Controlled-Animals-Project/main.js
+++ Sound-Controlled-Animals-Project/main.js
@@ -8,5 +8,31 @@ function startClassification(){
  }
  
  function gotResults(error, results){
-     console.log('got results')
- }
+    if(error){
+        console.error(error);
+    }
+    else{
+    console.log(results);
+    r=Math.floor(Math.random()*255)+1;
+    b=Math.floor(Math.random()*255)+1;
+    g=Math.floor(Math.random()*255)+1;
+    document.getElementById("result_label").innerHTML='I can Hear-'+results[0].label;
+    document.getElementById("result_confidence").innerHTML='Accuracy-'+(results[0].confidence*100).toFixed(2)+"%";
+    document.getElementById("result_label").style.color="rgb("+r+","+g+","+b+")";
+    document.getElementById("result_confidence").style.color="rgb("+r+","+g+","+b+")";
+    img=document.getElementById("TheImage.png");
+
+    if(results[0].label=="Mooing"){
+        img.src='OIP.jfif';
+    }
+    else if(results[0].label=="Meowing"){
+        img.src='R.png';
+    }
+    else if(results[0].label=="Barking"){
+        img.src='OIP(1).jfif';
+    }
+    else if(results[0].label=="Roaring"){
+        img.src='OIP(2).jfif';
+    }
+}
+}
