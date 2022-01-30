3b4f711f4b97ca2832c0dc7f4200ac881fc2011b
cskcsimha <92619094+cskcsimha@users.noreply.github.com>
2022-01-30 12:13:09 +0530

    Add files via upload

--- Sound-Controlled-Animals-Project/main.js
+++ Sound-Controlled-Animals-Project/main.js
@@ -7,6 +7,10 @@ function startClassification(){
      classifier.classify(gotResults);
  }
  
+ var dog=0;
+var cat=0;
+var cow=0;
+var lion=0;
  function gotResults(error, results){
     if(error){
         console.error(error);
@@ -16,23 +20,27 @@ function startClassification(){
     r=Math.floor(Math.random()*255)+1;
     b=Math.floor(Math.random()*255)+1;
     g=Math.floor(Math.random()*255)+1;
-    document.getElementById("result_label").innerHTML='I can Hear-'+results[0].label;
-    document.getElementById("result_confidence").innerHTML='Accuracy-'+(results[0].confidence*100).toFixed(2)+"%";
+    document.getElementById("result_confidence").innerHTML='Detected Dog-'+dog+" "+'Dectected Cat-'+cat+" "+'Dectected Cow-'+cow+" "+'Dectected Lion-'+lion;
+    document.getElementById("result_label").innerHTML='Detected voice is of-'+results[0].label;
     document.getElementById("result_label").style.color="rgb("+r+","+g+","+b+")";
     document.getElementById("result_confidence").style.color="rgb("+r+","+g+","+b+")";
-    img=document.getElementById("TheImage.png");
+    img=document.getElementById("alien1");
 
     if(results[0].label=="Mooing"){
         img.src='OIP.jfif';
+        cow=cow+1;
     }
     else if(results[0].label=="Meowing"){
         img.src='R.png';
+        cat=cat+1;
     }
     else if(results[0].label=="Barking"){
-        img.src='OIP(1).jfif';
+        img.src='OIP (1).jfif';
+        dog=dog+1;
     }
     else if(results[0].label=="Roaring"){
-        img.src='OIP(2).jfif';
+        img.src='OIP (2).jfif';
+        lion=lion+1;
     }
 }
 }
